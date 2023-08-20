318dc00285cd3e52c047b579b0ac5274c1f4ceb2
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2023-08-20 16:19:56 +0600

    Functionalities added

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-// // Toggling apply button conditionally
-// let inputFieldOfCoupon = document.getElementById("coupon-input");
-// let btnApply = document.getElementById("apply");
+// Toggling apply button conditionally
+let inputFieldOfCoupon = document.getElementById("coupon-input");
+let btnApply = document.getElementById("apply");
 
-// inputFieldOfCoupon.onkeyup = function () {
-//   let couponCode = inputFieldOfCoupon.value;
+inputFieldOfCoupon.onkeyup = function () {
+  let couponCode = inputFieldOfCoupon.value;
 
-//   if (couponCode === "SELL200") {
-//     btnApply.removeAttribute("disabled");
-//   } else {
-//     btnApply.setAttribute("disabled", "true");
-//   }
-// };
+  if (couponCode === "SELL200") {
+    btnApply.removeAttribute("disabled");
+  } else {
+    btnApply.setAttribute("disabled", "true");
+  }
+};
 
-// btnApply.addEventListener("click", function () {
-//   alert("Coupon applied successfully!");
-// });
+btnApply.addEventListener("click", function () {
+  alert("Coupon applied successfully!");
+});
 
-// //Adding product name and price to the sidebar
-// let product = document.querySelectorAll(".product");
-// let items = document.getElementById("items");
+//Adding product name and price to the sidebar
+let product = document.querySelectorAll(".product");
+let items = document.getElementById("items");
 
-// for (let i = 0; i < product.length; i++) {
-//   product[i].addEventListener("click", function () {
-//     let productName = document.querySelectorAll(".card-title");
-//     let li = document.createElement("li");
-//     li.classList.add("text-[12px]", "md:text-[20px]", "lg:text-[20px]", "py-1");
-//     li.textContent = productName[i].innerText;
-//     items.appendChild(li);
-//   });
-// }
+for (let i = 0; i < product.length; i++) {
+  product[i].addEventListener("click", function () {
+    let productName = document.querySelectorAll(".card-title");
+    let li = document.createElement("li");
+    li.classList.add("text-[12px]", "md:text-[20px]", "lg:text-[20px]", "py-1");
+    li.textContent = productName[i].innerText;
+    items.appendChild(li);
+  });
+}
